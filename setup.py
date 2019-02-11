2e6e5202785269559e34077ecdca4bc06c92716a
kumokiri2kdev <kumokiri2i@gmail.com>
2019-02-11 12:05:09 +0900

    [ADD]Packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='k2kvideo',
+    version='0.0.1',
+    packages=['k2kvideo'],
+    url='',
+    license='free',
+    author='kumokiri2k',
+    author_email='',
+    description='k2keiba JRA video RPA tools'
+)
